53bc6a0218276194ff383b68f068268cd07c4fc8
Fernando Cedeno <fernandostarlin1012@gmail.com>
2022-08-15 19:24:26 -0400

    The method get in our Class-Based view is now available to return a single movie data

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('movies/', MovieList.as_view(), name='movies'),
-    # path('movies/<str:pk>/', MovieDetail.as_view(), name='movie'),
+    path('movies/<str:pk>/', MovieList.as_view(), name='movie'),
     # path('create-movie/', MovieCreate.as_view(), name='create-movie'),
     # path('update-movie/<str:pk>/', MovieUpdate.as_view(), name='update-movie'),
     # path('delete-movie/<str:pk>/', MovieDelete.as_view(), name='delete-movie'),
--- api/views.py
+++ api/views.py
@@ -9,18 +9,36 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+
 class MovieList(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request):
-        movies = list(Movie.objects.values())
-        if len(movies) > 0:
-            data = {'message': "Success", 'movies': movies}
+    def get(self, request, pk):
+        """
+        Return a List of all Movies, or a Single movie
+        :param request:
+        :param pk:
+        :return:
+        """
+        if pk:
+            movies = list(Movie.objects.filter(pk=pk).values())
+            if len(movies) > 0:
+                movie = movies[0]
+                data = {'message': "Success", 'movie': movie}
+            else:
+                data = {'message': "Movie not found ..."}
+            return JsonResponse(data)
+
         else:
-            data = {'message': "Movies not found"}
-        return JsonResponse(data)
+
+            movies = list(Movie.objects.values())
+            if len(movies) > 0:
+                data = {'message': "Success", 'movies': movies}
+            else:
+                data = {'message': "Movies not found"}
+            return JsonResponse(data)
 
     def post(self, request):
 
