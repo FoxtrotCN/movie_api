2088bddb6390430debfe1d2508978058af567c73
Fernando Cedeno <fernandostarlin1012@gmail.com>
2022-08-15 15:41:43 -0400

    We miss a '/' inside a pk attribute in our app urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('movies/', MovieList.as_view(), name='movies'),
-    path('movies/<str:pk/>', MovieDetail.as_view(), name='movie'),
+    path('movies/<str:pk>/', MovieDetail.as_view(), name='movie'),
     path('create-movie/', MovieCreate.as_view(), name='create-movie'),
     path('update-movie/<str:pk>/', MovieUpdate.as_view(), name='update-movie'),
-    path('delete-movie/<str:pk/>', MovieDelete.as_view(), name='delete-movie'),
+    path('delete-movie/<str:pk>/', MovieDelete.as_view(), name='delete-movie'),
 ]
