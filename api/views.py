67e10cb17d891fcdba9eb1b4cbe1d3fa7682d423
Fernando Cedeno <fernandostarlin1012@gmail.com>
2022-08-15 15:37:41 -0400

    We've created our Class-Based views and setting our URLs

--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Movie
 
-# Create your views here.
+
+class MovieList(ListView):
+    model = Movie
+
+
+class MovieDetail(DetailView):
+    model = Movie
+
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = ['title', 'synopsis', 'genre']
+    success_url = reverse_lazy('movies')
+
+
+class MovieUpdate(UpdateView):
+    model = Movie
+    fields = ['title', 'synopsis', 'genre']
+    success_url = reverse_lazy('movie')
+
+
+class MovieDelete(DeleteView):
+    model = Movie
+    context_object_name = 'movie'
+    success_url = reverse_lazy('movies')
