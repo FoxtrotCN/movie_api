5a1be4d323638bae287824d5dcd0443384ee610d
Fernando Cedeno <fernandostarlin1012@gmail.com>
2022-08-15 15:16:13 -0400

    The model for our API, was created succesfully.

---
+++ api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    ACTION = 'AC'
+    DRAMA = 'DR'
+    COMEDY = 'COMEDY'
+    SCIENCE_FICTION = 'SF'
+    THRILLER = 'TR'
+    RELIGIOUS = 'RG'
+
+    GENRE_CHOICES = [
+
+        (ACTION, 'Accion'),
+        (DRAMA, 'Drama'),
+        (COMEDY, 'Comedy'),
+        (SCIENCE_FICTION, 'Ciencia Ficcion'),
+        (THRILLER, 'Triler'),
+        (RELIGIOUS, 'Religioso')
+    ]
+
+    title = models.CharField(max_length=155, blank=False)
+    synopsis = models.TextField(max_length=1000, blank=True)
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICES, default='', blank=False)
+
+    def __str__(self):
+        return self.title
